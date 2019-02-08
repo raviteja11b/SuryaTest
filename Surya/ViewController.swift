e953e2625d4f8c6a6a4ba3a491192140a5c59ed6
raviteja <raviteja.buragadda@brongo.in>
2019-02-08 16:45:02 +0530

    Initial Commit

---
+++ Surya/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Surya
+//
+//  Created by RaviTeja on 08/02/19.
+//  Copyright © 2019 RaviTeja. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
